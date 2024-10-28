cb1d973433ae21a79a66f6fffa25a84af71a6654
Hunter Nisonoff <hunter_nisonoff@berkeley.edu>
2024-10-28 16:32:04 -0700

    Clean up file

--- applications/inverse_folding/utils/train_full_stability_model.py
+++ applications/inverse_folding/utils/train_full_stability_model.py
@@ -49,73 +49,76 @@ def parse_args():
     return parser.parse_args()
 
 
-args = parse_args()
-device = 'cuda'
-rocklin_df = process_rocklin_data()
-name_to_graph = torch.load(
-    ROCKLIN_DIR / 'name_to_graph.pt'
-)
+if __name__ == "__main__":
+    args = parse_args()
+    device = 'cuda'
+    print("Loading Rocklin Data...")
+    rocklin_df = process_rocklin_data()
+    name_to_graph = torch.load(
+        ROCKLIN_DIR / 'name_to_graph.pt'
+        , weights_only=False)
 
-train_ds = rocklin_df_to_dataset(rocklin_df)
-loader_train = StructureLoader(train_ds, batch_size=6000 * 2)
+    print("Creating Rocklin PyTorch dataset")
+    train_ds = rocklin_df_to_dataset(rocklin_df)
+    loader_train = StructureLoader(train_ds, batch_size=6000 * 2)
 
 
-model = FlowMatchPMPNN(node_features=args.hidden_dim,
-                       edge_features=args.hidden_dim,
-                       hidden_dim=args.hidden_dim,
-                       num_encoder_layers=args.num_encoder_layers,
-                       num_decoder_layers=args.num_encoder_layers,
-                       k_neighbors=args.num_neighbors,
-                       dropout=args.dropout,
-                       augment_eps=args.backbone_noise,
-                       num_letters=21,
-                       vocab=21)
-stability_model = StabilityPMPNN(model)
-stability_model.load_fm_mpnn_weights(
-    args.init_model_path
-)
-stability_model.to(device)
+    model = FlowMatchPMPNN(node_features=args.hidden_dim,
+                           edge_features=args.hidden_dim,
+                           hidden_dim=args.hidden_dim,
+                           num_encoder_layers=args.num_encoder_layers,
+                           num_decoder_layers=args.num_encoder_layers,
+                           k_neighbors=args.num_neighbors,
+                           dropout=args.dropout,
+                           augment_eps=args.backbone_noise,
+                           num_letters=21,
+                           vocab=21)
+    stability_model = StabilityPMPNN(model)
+    stability_model.load_fm_mpnn_weights(
+        args.init_model_path
+    )
+    stability_model.to(device)
 
-train_lr = 1e-4
-weight_decay = 1e-4
-adam_betas = (0.9, 0.99)
-opt = AdamW(stability_model.parameters(),
-            lr=train_lr,
-            betas=adam_betas,
-            weight_decay=weight_decay)
+    train_lr = 1e-4
+    weight_decay = 1e-4
+    adam_betas = (0.9, 0.99)
+    opt = AdamW(stability_model.parameters(),
+                lr=train_lr,
+                betas=adam_betas,
+                weight_decay=weight_decay)
 
-criterion = torch.nn.MSELoss()
+    criterion = torch.nn.MSELoss()
 
-for epoch in range(10):
-    stability_model.train()
-    train_pbar = tqdm(loader_train)
-    losses_per_batch = np.zeros(len(loader_train))
-    train_ys = []
-    train_yhats = []
+    for epoch in range(10):
+        stability_model.train()
+        train_pbar = tqdm(loader_train)
+        losses_per_batch = np.zeros(len(loader_train))
+        train_ys = []
+        train_yhats = []
 
-    for batch_idx, batch in enumerate(train_pbar):
-        opt.zero_grad()
-        X, S1, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(
-            batch, device)
-        for b in batch:
-            b['seq'] = b['wt']
-            b['seq_chain_A'] = b['wt']
-        X, S1_wt, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(
-            batch, device)
-        y = torch.tensor([b['y'] for b in batch]).to(device)
-        wt_y = torch.tensor([b['wt_dG'] for b in batch]).to(device)
-        yhat = stability_model(X, S1, mask, chain_M, residue_idx,
-                               chain_encoding_all).reshape(-1)
-        yhat_wt = stability_model(X, S1_wt, mask, chain_M, residue_idx,
-                                  chain_encoding_all).reshape(-1)
-        loss = criterion(yhat - yhat_wt, y - wt_y)
-        #loss = criterion(yhat, batch.y)
-        loss.backward()
-        opt.step()
-        losses_per_batch[batch_idx] = loss.item()
-        train_pbar.set_postfix(loss=losses_per_batch[:batch_idx].mean())
-        train_ys.append(y.cpu().detach().numpy())
-        train_yhats.append(yhat.cpu().detach().numpy())
+        for batch_idx, batch in enumerate(train_pbar):
+            opt.zero_grad()
+            X, S1, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(
+                batch, device)
+            for b in batch:
+                b['seq'] = b['wt']
+                b['seq_chain_A'] = b['wt']
+            X, S1_wt, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(
+                batch, device)
+            y = torch.tensor([b['y'] for b in batch]).to(device)
+            wt_y = torch.tensor([b['wt_dG'] for b in batch]).to(device)
+            yhat = stability_model(X, S1, mask, chain_M, residue_idx,
+                                   chain_encoding_all).reshape(-1)
+            yhat_wt = stability_model(X, S1_wt, mask, chain_M, residue_idx,
+                                      chain_encoding_all).reshape(-1)
+            loss = criterion(yhat - yhat_wt, y - wt_y)
+            #loss = criterion(yhat, batch.y)
+            loss.backward()
+            opt.step()
+            losses_per_batch[batch_idx] = loss.item()
+            train_pbar.set_postfix(loss=losses_per_batch[:batch_idx].mean())
+            train_ys.append(y.cpu().detach().numpy())
+            train_yhats.append(yhat.cpu().detach().numpy())
 
-    torch.save(stability_model.state_dict(),
-               f'stability_all_checkpoint_{epoch}.pt')
+        torch.save(stability_model.state_dict(),
+                   f'stability_all_checkpoint_{epoch}.pt')
